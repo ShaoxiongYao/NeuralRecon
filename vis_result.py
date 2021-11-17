8dde2ef0e1f63da3c742f4655a43ad15254b214d
Shaoxiong Yao <syao16@illinois.edu>
2021-11-17 15:10:29 -0600

    clean dataset

--- vis_result.py
+++ vis_result.py
@@ -1,7 +1,7 @@
 import open3d as o3d
 
 if __name__ == '__main__':
-    mesh = o3d.io.read_triangle_mesh("results/scene_demo_checkpoints_fusion_eval_47/gradLoungeHaomengDynamicPerson.ply")
+    mesh = o3d.io.read_triangle_mesh("results/scene_demo_checkpoints_fusion_eval_47/gradLoungeHaomengStaticPerson.ply")
 
     print("Try to render a mesh with normals (exist: " +
           str(mesh.has_vertex_normals()) + ") and colors (exist: " +
