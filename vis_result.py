c18db97fe74f894bf6a724a54972e4afca550eec
Shaoxiong Yao <syao16@illinois.edu>
2021-12-05 15:50:08 -0600

    add failure case to use mask in image

--- vis_result.py
+++ vis_result.py
@@ -2,7 +2,7 @@ import numpy as np
 import open3d as o3d
 
 if __name__ == '__main__':
-    mesh = o3d.io.read_triangle_mesh("results/scene_demo_checkpoints_fusion_eval_47/RestAreawithPerson_mask_feature.ply")
+    mesh = o3d.io.read_triangle_mesh("results/scene_demo_checkpoints_fusion_eval_47/slowPersonMove_mask_in_features.ply")
 
     # RestAreawithPerson_mask_feature.ply
     # RestAreawithPerson_mask_image_mean.ply
@@ -11,21 +11,20 @@ if __name__ == '__main__':
 
     print("Try to render a mesh with normals (exist: " +
           str(mesh.has_vertex_normals()) + ") and colors (exist: " +
-          str(mesh.has_vertex_colors()) + ")")
-      
-    # o3d.visualization.draw_geometries([mesh])
-    print("A mesh with no normals and no colors does not seem good.")
+          str(mesh.has_vertex_colors()) + ")")      
 
-    theta = np.deg2rad(15)  
     print("Computing normal and rendering it.")
     mesh.compute_vertex_normals()
-#     mesh.paint_uniform_color([251/255.0,189/255.0,128/255.0])
     o3d.visualization.draw_geometries([mesh], 
-                                          zoom=0.3,
-                                          front=[0, -np.cos(theta), np.sin(theta)],
-                                          lookat=[0.5, 0.5, 0.5],
-                                          up=[0, 0, 1])
+                                      zoom=0.1,
+                                      front=[ -0.99630886767291027, 0.046752485339995403, 0.071991980878825371 ],
+                                      lookat=[ 0.50564805391588863, 0.72946279697650085, 1.1386682797845395 ],
+                                      up=[ 0.071195739086218277, -0.018480705951851827, 0.99729114617722758 ])
 
-#     print("Painting the mesh")
-#     mesh.paint_uniform_color([1, 0.706, 0])
-#     o3d.visualization.draw_geometries([mesh])
+# old visualization parameters
+#     theta = np.deg2rad(15)  
+#     o3d.visualization.draw_geometries([mesh], 
+#                                       zoom=0.3,
+#                                       front=[0, -np.cos(theta), np.sin(theta)],
+#                                       lookat=[0.5, 0.5, 0.5],
+#                                       up=[0, 0, 1])
